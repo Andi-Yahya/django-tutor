506c9fd8e88835c478169e7558c52cacbc0cc48a
Andi-Yahya <andiyahya675@gmail.com>
2023-03-18 21:25:59 +0700

    membuat template variabel pada python sehingga tidak perlu mengulangi pembuatan file template jika keduanya hampir sama (diubah via variabel)

--- about/views.py
+++ about/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,"about.html")  #pastikan tanda petik harus sama dengan tanda petik yang ada di library urls bagian urlPatterns
+    context = {
+        'nama':'Andi Yahya',
+        'jurusan':'Teknik Informatika'
+    }
+    return render(request,"about/index.html",context)  #pastikan tanda petik harus sama dengan tanda petik yang ada di library urls bagian urlPatterns di dalam parameter dari Include() nya
+
+
+def me(request):
+    context = {
+        'nama':'Ucup Siregar',
+        'jurusan':'Sistem Tatanan DPR'
+    }
+    return render(request,'about/index.html',context)
