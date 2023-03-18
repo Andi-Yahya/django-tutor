6a29dbd3e361a9b6930bd7dd869e3fbac66744de
Andi-Yahya <andiyahya675@gmail.com>
2023-03-18 09:17:15 +0700

    belajar templating pada APP django dan membuat sub-url didalamnya

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,10 @@ from django.urls import path #django 4.0
 from . import views 
 
 urlpatterns =[
-    path("recent/",views.recent), #TIPS pastikan pada saat routing ke sub-url dari app alamat yang dituju di chrome harus sama agar tidak terjadi error dengan cara mwngisi path sub-url nya seperti pada sub-url ini = path("recent/"..
-    path("",views.index)
+    path("recent/",views.recent), #TIPS pastikan pada saat routing ke sub-url dari app alamat yang dituju di chrome harus sama agar tidak terjadi error dengan cara mengisi path sub-url nya seperti pada sub-url ini = path("recent/"..
+    #url patterns untuk recent ini harus dibuat seperti ini karena melanjutkan url dari app utama yaitu django_project
+    path("",views.index),
+    path("recent/url",views.recent),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,"blog.html")
+    return render(request,"blog/blog.html") #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu blog.html
 
 def recent(request):
-    return render(request, "recent.html")
+    return render(request, "blog/recent.html") #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu recent.html
