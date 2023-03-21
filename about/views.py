f22233200c8ff801e61f7dfea469f362897eb810
Andi-Yahya <andiyahya675@gmail.com>
2023-03-21 21:53:54 +0700

    lanjooot STATIC file dan menambahkan gambar

--- about/views.py
+++ about/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 def index(request):
     context = {
         'nama':'Andi Yahya',
-        'jurusan':'Teknik Informatika'
+        'jurusan':'Teknik Informatika',
+        'img':'about/img/banner_about.png'
     }
-    return render(request,"about/index.html",context)  #pastikan tanda petik harus sama dengan tanda petik yang ada di library urls bagian urlPatterns di dalam parameter dari  Include() nya
+    # return render(request,"about.index.html",context)  #pastikan tanda petik harus sama dengan tanda petik yang ada di library urls bagian urlPatterns di dalam parameter dari  Include() nya
+    return render(request,"index.html",context) #menggunakan cara templating satu file sehingga key img akan terkirim ke file index umum di index pada templates
 
 
 def me(request):
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,"blog/blog.html") #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu blog.html
+    context = {
+        'judul':'ini adalah blog',
+        'subjudul':'ini adalah BLOG dari web django',
+        'img':'blog/img/banner_blog.png',
+    }
+
+    # return render(request,"blog/blog.html",context) #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu blog.html
+    return render(request,"index.html",context) #menggunakan cara templating satu file sehingga key img akan terkirim ke file index umum di index pada templates
 
 def recent(request):
     return render(request, "blog/recent.html") #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu recent.html
