842bb8908a67251aa40d4e55124422f0f4aa365b
Andi-Yahya <andiyahya675@gmail.com>
2023-03-22 22:32:38 +0700

    Static file dan CSS

--- about/views.py
+++ about/views.py
@@ -6,7 +6,9 @@ def index(request):
     context = {
         'nama':'Andi Yahya',
         'jurusan':'Teknik Informatika',
-        'img':'about/img/banner_about.png'
+        'subjudul': 'I LOVE DJango so Much from now on, need to knuckle down on it',  
+        'img':'about/img/banner_about.png',
+        'app_css':'about/css/style.css',
     }
     # return render(request,"about.index.html",context)  #pastikan tanda petik harus sama dengan tanda petik yang ada di library urls bagian urlPatterns di dalam parameter dari  Include() nya
     return render(request,"index.html",context) #menggunakan cara templating satu file sehingga key img akan terkirim ke file index umum di index pada templates
--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,16 @@ def index(request):
         'judul':'ini adalah blog',
         'subjudul':'ini adalah BLOG dari web django',
         'img':'blog/img/banner_blog.png',
+        'app_css':'blog/css/styles.css',
     }
 
     # return render(request,"blog/blog.html",context) #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu blog.html
     return render(request,"index.html",context) #menggunakan cara templating satu file sehingga key img akan terkirim ke file index umum di index pada templates
 
 def recent(request):
-    return render(request, "blog/recent.html") #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu recent.html
+    context = {
+        'judul':'ini adalah Recent',
+        'subjudul':'ini adalah Recent dari Blog web django',
+        'app_css2':'blog/css/styles2.css', #ini adalah cara untuk menggunakan static, sehingga pada file yang dituju hanya memerlukan variabelnya saja
+    }
+    return render(request, "index.html",context) #ini untuk menunjukkan dimana lokasi templates, yang mana lokasinya saat ini adalah berada di sub folder templates yang bernama blog dan file didalamnya nya yaitu recent.html
